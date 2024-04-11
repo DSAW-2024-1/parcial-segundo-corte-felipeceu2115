d32e6779a3b5dadd0e7e484e3ce92d26afb99223
Felipeceu21 <juanceur@unisabana.edu.co>
2024-04-11 17:28:21 -0500

    3

--- routes/coin.js
+++ routes/coin.js
@@ -4,3 +4,19 @@ import fetch from 'node-fetch';
 const API_Moneda = 'https://api.coincap.io/v2/assets/';
 
 const router = express.Router();
+
+router.get('/:coinName', async (req, res) => {
+    const { coinName } = req.params;
+  
+    try {
+      const response = await fetch(`${API_Moneda}${coinName}`);
+      const data = await response.json();
+      const precio = data.data.priceUsd;
+  
+      res.send(`El precio en dólares de la moneda ${coinName} para el día de hoy es ${precio}`);
+    } catch (error) {
+      res.status(502).send('El nombre de la moneda no fue encontrado en la base de datos');
+    }
+  });
+  
+  export default router;
